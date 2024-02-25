07d86ef9a9ae43f2f2247e22664ee58f7483b723
volihor <ihor.voloshyn@gmail.com>
2024-02-25 23:39:34 +0200

    Added Logger Log4j2. Create logs for different levels of event working with bugs.

--- src/main/java/global/goit/Main.java
+++ src/main/java/global/goit/Main.java
@@ -1,32 +1,19 @@
 package global.goit;
 
-
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import java.util.logging.Logger;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-//    Logger logger = Logger.getLogger(getClass().getName());
-//    Logger loggerSlf4j = LoggerFactory.getLogger(Main.class);
-//    private static Logger loggerSlf4j = LoggerFactory.getLogger(Main.class);
 private static Logger log4j = LogManager.getLogger(Main.class);
-
     public static void main(String[] args) {
-        Main main = new Main();
-        main.getLoggerTest();
-        System.out.println("---   hi   ---");
-//        Logger loggerSLF4J = LoggerFactory.getLogger(Main.class);
-//        loggerSLF4J.info("Hi from loggerSLF4J ))");
-//        loggerSlf4j.info("Hi from loggerSLF4J ))");
-    }
 
-    private void getLoggerTest() {
-        int numForTest = 1234;
-//        logger.info("My Message numForTest {}");
-//        logger.info("My Message: \'java.util.logging.Logger\' can NOT add fields to logger: test {}\", numForTest");
-//        loggerSlf4j.info("My Message: \'org.slf4j.Logger\' CAN add fields to logger: test {} : ", numForTest);
+        log4j.info("---   Test from Logger   ---");
+        log4j.info("===  Hello from log4j, \'add number test\': {} ===", 444);
+
+        log4j.debug(" => Debug log message");
+        log4j.info (" => Info log message" );
+        log4j.error(" => Error log message");
+        log4j.warn (" => Warn log message" );
+
     }
 }
